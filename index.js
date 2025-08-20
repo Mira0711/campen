1f9faee1f46479b1b962bdeafae02e7c0d8c64b4
Mira0711 <moritz.hiller@gmx.de>
2025-08-20 14:00:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ import { getDatabase, ref, get, set, onValue } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyANnB_kd111pGYAG-YkE8_4KZ6ZE3gOK44",
   authDomain: "campen-647d2.firebaseapp.com",
-  databaseURL: "https://campen-647d2-default-rtdb.firebaseio.com",
+  databaseURL: "https://campen-647d2-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "campen-647d2",
   storageBucket: "campen-647d2.appspot.com",
   messagingSenderId: "985517833842",
   appId: "1:985517833842:web:f2f78370df65716fbbcceb"
 };
-
 // 2️⃣ Firebase initialisieren
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
